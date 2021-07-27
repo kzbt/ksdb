fab6b1536e0bdc658feacec2990eb6ac3412e81f
Kiran Sebastian <kiranjoysebastian@gmail.com>
2021-07-27 22:23:40 +0800

    Add disk manager tests

--- src/storage/disk.rs
+++ src/storage/disk.rs
@@ -50,3 +50,51 @@ impl<'a> DiskManager for DefaultDiskManager<'a> {
         Ok(buf.to_vec())
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{fs::OpenOptions, ops::Range};
+
+    use super::*;
+
+    fn pad_zeros(bytes: &[u8], len: usize) -> Vec<u8> {
+        let mut padding = vec![0; len - bytes.len()];
+        let mut result = bytes.to_vec();
+
+        result.append(&mut padding);
+        result
+    }
+
+    #[test]
+    fn should_read_write_correctly() {
+        let db_file = OpenOptions::new()
+            .create(true)
+            .write(true)
+            .read(true)
+            .open("/tmp/db")
+            .unwrap();
+        let mut disk_manager = DefaultDiskManager::new(&db_file);
+        disk_manager
+            .write_page(PageId(2), "some page content".as_bytes().to_vec())
+            .unwrap();
+        disk_manager
+            .write_page(PageId(5), "another page content".as_bytes().to_vec())
+            .unwrap();
+
+        let data1 = disk_manager.read_page(PageId(1)).unwrap();
+        let data2 = disk_manager.read_page(PageId(2)).unwrap();
+        let data4 = disk_manager.read_page(PageId(4)).unwrap();
+        let data5 = disk_manager.read_page(PageId(5)).unwrap();
+
+        assert_eq!(data1, pad_zeros(&vec![], PAGE_SIZE as usize));
+        assert_eq!(
+            data2,
+            pad_zeros("some page content".as_bytes(), PAGE_SIZE as usize)
+        );
+        assert_eq!(data4, pad_zeros(&vec![], PAGE_SIZE as usize));
+        assert_eq!(
+            data5,
+            pad_zeros("another page content".as_bytes(), PAGE_SIZE as usize)
+        );
+    }
+}
